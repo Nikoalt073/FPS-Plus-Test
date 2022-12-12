cf9f1f49ea17d33f55f45f155b3cbf260b226554
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-12 22:54:39 +0200

    Update TitleVideo.hx

--- source/title/TitleVideo.hx
+++ source/title/TitleVideo.hx
@@ -8,51 +8,29 @@ using StringTools;
 
 class TitleVideo extends FlxState
 {
-	var oldFPS:Int = VideoHandler.MAX_FPS;
-	var video:VideoHandler;
-	var titleState = new TitleScreen();
-
 	override public function create():Void
 	{
-
 		super.create();
 
-		FlxG.sound.cache(Paths.music("klaskiiLoop"));
+		Paths.music("klaskiiLoop");
 
-		if(!Main.novid){
-
-			VideoHandler.MAX_FPS = 60;
-
-			video = new VideoHandler();
-
-			video.playMP4(Paths.video('klaskiiTitle'), function(){
-				next();
-				#if web
-					VideoHandler.MAX_FPS = oldFPS;
-				#end
-			}, false, true);
-
-			add(video);
-			
+		if (!Main.novid)
+		{
+			var video:VideoHandler = new VideoHandler();
+			video.finishCallback = next;
+			video.playVideo(SUtil.getStorageDirectory() + Paths.video('klaskiiTitle'));
 		}
-		else{
+		else
+		{
 			next();
 		}
 	}
 
-	override public function update(elapsed:Float){
-
-		super.update(elapsed);
-
-	}
-
-	function next():Void{
-
+	function next():Void
+	{
 		FlxG.camera.flash(FlxColor.WHITE, 60);
 		FlxG.sound.playMusic(Paths.music("klaskiiLoop"), 0.75);
 		Conductor.changeBPM(158);
-		FlxG.switchState(titleState);
-
+		FlxG.switchState(new TitleScreen());
 	}
-	
 }
