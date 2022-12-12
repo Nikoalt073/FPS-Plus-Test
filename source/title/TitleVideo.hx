e3906dee8f4ff80be6681db5f061e4b39056bef1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-12 22:56:24 +0200

    Update TitleVideo.hx

--- source/title/TitleVideo.hx
+++ source/title/TitleVideo.hx
@@ -18,8 +18,7 @@ class TitleVideo extends FlxState
 		{
 			var video:VideoHandler = new VideoHandler();
 			video.finishCallback = next;
-			video.playVideo(SUtil.getStorageDirectory() + Paths.video('klaskiiTitle'));
-		}
+			video.playVideo(Paths.video('klaskiiTitle'));
 		else
 		{
 			next();
