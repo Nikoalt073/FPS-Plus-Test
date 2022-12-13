edece3fe09fdacd7dd2b55f05490ddce696019bd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-13 22:55:08 +0200

    Update TitleScreen.hx

--- source/title/TitleScreen.hx
+++ source/title/TitleScreen.hx
@@ -115,6 +115,12 @@ class TitleScreen extends MusicBeatState
 
 		var pressedEnter:Bool = controls.ACCEPT || controls.PAUSE;
 
+		#if mobile
+		for (touch in FlxG.touches.list)
+			if (touch.justPressed)
+				pressedEnter = true;
+		#end
+
 		if (!transitioning && controls.BACK)
 		{
 			System.exit(0);
