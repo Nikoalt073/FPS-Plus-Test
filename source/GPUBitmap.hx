546ab979c3ea5eb1a4ff579baf9c1e6a34e5ae34
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-13 06:08:43 +0200

    HUH

--- source/GPUBitmap.hx
+++ source/GPUBitmap.hx
@@ -11,7 +11,7 @@ import openfl.utils.Assets;
 **/
 class GPUBitmap
 {
-	private static var IAssetCache:Map<String, Texture> = [];
+	private static var textureCache:Map<String, Texture> = [];
 
 	/**
 	 * Creates a `BitmapData` for a `IMAGE` and deletes the reference stored in RAM leaving only the texture in VRAM.
