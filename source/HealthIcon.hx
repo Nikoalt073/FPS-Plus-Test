2a4e424db6be99aa5255e57bd6db2deadba02dd0
Nikoalt073 <119976044+Nikoalt073@users.noreply.github.com>
2022-12-20 23:48:43 +0000

    Maybe fix null exception

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -26,6 +26,7 @@ class HealthIcon extends FlxSprite
 		animation.add('bf-car', [0, 1, 30], 0, false, isPlayer);
 		animation.add('bf-christmas', [0, 1, 30], 0, false, isPlayer);
 		animation.add('bf-pixel', [21, 41, 40], 0, false, isPlayer);
+		animation.add('bf-pixel-enemy', [21, 41, 40], 0, false, isPlayer);
 		animation.add('spooky', [2, 3, 31], 0, false, isPlayer);
 		animation.add('pico', [4, 5, 32], 0, false, isPlayer);
 		animation.add('mom', [6, 7, 33], 0, false, isPlayer);
